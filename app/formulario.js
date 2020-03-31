a3a04bc5550f6ec42ced57579e5a5a27f530d53e
arangojd26 <arangojd26@gmail.com>
2020-03-30 21:23:23 -0500

    Commit agregando el modal de agregar nombres con la base de datos ya configurada

---
+++ app/formulario.js
@@ -0,0 +1,30 @@
+$("#formulario").submit(function(event){
+    event.preventDefault(); //Almacena los datos sin refrescar el sitio web.
+    enviar();
+});
+
+function enviar(){
+    //console.log("ejecutado");
+    var datos = $("#formulario").serialize(); //Toma los datos name y los guarda en un arreglo.
+    $.ajax({ //Envía los datos a formulario php, sin actualizar la pagina.
+        type: "post",
+        url:"app/formulario.php",
+        data: datos,
+        success: function(texto){
+            if(texto=="exito"){
+                correcto();
+            }else{
+                phpError(texto);  
+            }
+        }
+    })
+}
+
+function correcto(){
+    $("#mensajeExito").removeClass("d-none");
+    $("#mensajeError").addClass("d-none");
+}
+function phpError(texto){
+    $("#mensajeError").removeClass("d-none");
+    $("#mensajeError").html(texto);
+}
